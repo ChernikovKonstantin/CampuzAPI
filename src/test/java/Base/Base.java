3d2516cceff009b2d28802d0b17d7ce23cfcd51b
Qasquad1 <chernikov.k.qa@gmail.com>
2022-09-29 15:42:06 +0700

    тест пуша в гит

--- src/test/java/Base/Base.java
+++ src/test/java/Base/Base.java
@@ -3,7 +3,7 @@ package Base;
 import io.restassured.response.Response;
 import net.minidev.json.JSONObject;
 
-import java.util.Random;
+import java.util.Random; //тест пуша
 
 import static io.restassured.RestAssured.given;
 
